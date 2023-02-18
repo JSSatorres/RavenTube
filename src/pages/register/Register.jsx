0cd3b756e330a1147a8927eb3b2c540bf4f99417
jssatorres <juansataz.dev@gmail.com>
2023-02-18 17:37:55 +0100

    git (pages) login register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,31 +1,28 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect } from 'react'
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Formik } from 'formik';
 import { useSelector, useDispatch } from 'react-redux'
 // import { onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import logoRavenLoop from '../../assets/image/logoRavenLoop.png'
-import { login } from '../../config/firebase';
-import loginSchema from './loginSchema';
-// import { useLoginMutation } from '../../store/api/authApi';
+import { register } from '../../config/firebase';
+import registerSchema from './registerSchema';
+// import {registerMutation } from '../../store/api/authApi';
 // import { useGetTodosQuery } from '../../store/api/todosApi';
 // import { auth, login } from '../../config/firebase'
 // import { setAuthorization } from '../../store/slices/authSlice';
 
-const Login = () => {
+const Register = () => {
   // const dispatch = useDispatch()
   // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
   const navigate = useNavigate()
 
   const submitForm = async ({ email, password }) => {
     try {
-      await login(email, password);
+      await register(email, password);
       navigate('/home')
-      toast.success('Action completed successfully!')
     } catch (error) {
-      toast.error('Login failled!')
+      throw new Error()
     }
   }
   // const { data:todo } = useGetTodosQuery()
@@ -34,16 +31,15 @@ const Login = () => {
   return (
     <section className='bg-gradient-to-r from-black via-gray-800 to-black h-screen'>
       <div className='mx-auto flex flex-col items-center justify-center px-6 py-8 '>
-        <ToastContainer />
         <div className='mb-6 flex items-center text-2xl font-semibold'>
           <img className='mx-auto h-20 w-auto' src={logoRavenLoop} alt='logo-RavenLoop' />
         </div>
         <div className='w-full rounded-lg shadow dark:border sm:max-w-md md:mt-0 xl:p-0 bg-gray-900  border border-rblue'>
           <div className='space-y-4 p-6 sm:p-8 md:space-y-6'>
-            <h1 className='text-xl font-bold leading-tight tracking-tight  md:text-2xl'>Sign in to your account</h1>
+            <h1 className='text-xl font-bold leading-tight tracking-tight  md:text-2xl'>Register an account</h1>
             <Formik
               initialValues={{ email: '', password: '' }}
-              validationSchema={loginSchema}
+              validationSchema={registerSchema}
               onSubmit={(values) => {
                 submitForm(values)
               }}
@@ -88,7 +84,7 @@ const Login = () => {
                       className='btn-primary my-4'
                       type='submit'
                     >
-                      Sign in
+                      Register
                     </button>
                   </div>
                 </form>
@@ -97,9 +93,8 @@ const Login = () => {
           </div>
         </div>
       </div>
-
     </section>
   )
 }
 
-export default Login
+export default Register
