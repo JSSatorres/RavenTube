383efe7087bf40f1c8d35b7f81133f549822aa29
jssatorres <juansataz.dev@gmail.com>
2023-02-18 17:41:54 +0100

    Feat (config) store and toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify';
 import Router from './Router'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Router />
+    <>
+      <Router />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+    </>
   )
 }
 
