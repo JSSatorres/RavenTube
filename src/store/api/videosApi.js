d1c04563a3d5e218fe55ff902174ba3411626079
jssatorres <juansataz.dev@gmail.com>
2023-02-19 12:26:29 +0100

    feat (store) Api videos RTK query

--- src/store/api/videosApi.js
+++ src/store/api/videosApi.js
@@ -2,18 +2,29 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const videosApi = createApi({
   reducerPath: 'videos',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000',
+    mode: 'cors',
+    prepareHeaders: (headers) => {
+      headers.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+      headers.set('Access-Control-Expose-Headers', 'X-Total-Count')
+
+      console.log('Headers:', headers);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getVideos: builder.query({
-      query: () => ({
-        url: '/videos',
-        // params: {
-        //   part: 'snippet',
-        //   q: query,
-        //   key: import.meta.env.VITE_YOUTUBE_API_KEY,
-        //   type: 'video',
-        //   maxResults: 12,
-        // },
+      query: (seacrh = '', page = 1, limit = 12) => ({
+        url: `/videos?title_like=${seacrh}&_page=${page}&_limit=${limit}`,
+        transformResponse: (response) => {
+          const totalCount = response.headers.get('X-Total-Count')
+          console.log('por ue no funcionooo');
+          console.log('response:', response)
+          const data = response.json()
+          console.log('data:', data)
+          return { totalCount, data }
+        },
         providesTags: ['Videos'],
       }),
     }),
