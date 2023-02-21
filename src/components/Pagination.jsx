be051aa2189f38b8d9a569cc2dd063cbe96e25e5
jssatorres <juansataz.dev@gmail.com>
2023-02-21 13:35:34 +0100

    Fit (styles) adding styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  console.log('currentPage, totalPages', currentPage, totalPages);
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
-  console.log('pageNumbers', pageNumbers);
 
   return (
-    <div className='flex flex-row justify-center my-6'>
+    <div className='flex flex-row justify-center  bg-greydark py-5'>
       <nav>
         <ul className='pagination flex flex-row gap-x-2'>
           <li className={`${currentPage === 1 ? 'hidden' : ''}`}>
             <button
-              className='px-3 py-1 rounded-md bg-gray-800 text-white hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-300 disabled:opacity-50'
+              className='custom-pagination-button'
               onClick={() => onPageChange(currentPage - 1)}
               disabled={currentPage === 1}
               type='button'
@@ -23,7 +21,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
           {pageNumbers.map((number) => (
             <li key={number} className={`${currentPage === number ? 'active' : ''}`}>
               <button
-                className='px-3 py-1 rounded-md bg-gray-800 text-white hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-300'
+                className={`custom-pagination-button ${number === currentPage && 'bg-gray-900 '}`}
                 onClick={() => onPageChange(number)}
                 type='button'
               >
@@ -34,7 +32,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 
           <li className={`${currentPage === totalPages ? 'hidden' : ''}`}>
             <button
-              className='px-3 py-1 rounded-md bg-gray-800 text-white hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-300 disabled:opacity-50'
+              className='custom-pagination-button'
               onClick={() => onPageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
               type='button'
