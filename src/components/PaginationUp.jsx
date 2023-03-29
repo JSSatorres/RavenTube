cb6f40dd9a6185f8cb5c9dfb28e289d671b8bd04
jssatorres <juansataz.dev@gmail.com>
2023-03-29 18:34:06 +0200

    Feat (PaginationUP) add omponent

---
+++ src/components/PaginationUp.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const PaginationUp = ({
+  currentPage, onPageChange, totalItems, itemsPerPage,
+}) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className='flex flex-row justify-center  bg-greydark py-5'>
+      <nav>
+        <ul className='pagination flex flex-row gap-x-2'>
+          <li>
+            <button
+              className='custom-pagination-button'
+              onClick={() => onPageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              type='button'
+            >
+              Previous
+            </button>
+          </li>
+          {pageNumbers.map((number) => (
+            <li key={number} className={`${currentPage === number ? 'active' : ''}`}>
+              <button
+                className={`custom-pagination-button ${number === currentPage && 'bg-gray-900 '}`}
+                onClick={() => onPageChange(number)}
+                type='button'
+              >
+                {number}
+              </button>
+            </li>
+          ))}
+          <li>
+            <button
+              className='custom-pagination-button'
+              onClick={() => onPageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              type='button'
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default PaginationUp;
